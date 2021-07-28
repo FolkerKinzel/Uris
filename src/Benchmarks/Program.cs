52521c00ee5671ffccb7cddca7ebfa71f74ad6e6
Folker Kinzel <fkinzel@freenet.de>
2021-07-28 17:28:22 +0200

    Benchmarks

--- src/Benchmarks/Program.cs
+++ src/Benchmarks/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BenchmarkDotNet.Running;
 
 namespace Benchmarks
 {
@@ -6,7 +7,7 @@ namespace Benchmarks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var summary = BenchmarkRunner.Run<UrisBench>();
         }
     }
 }
--- src/Benchmarks/UrisBench.cs
+++ src/Benchmarks/UrisBench.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,30 @@ namespace Benchmarks
     [MemoryDiagnoser]
     public class UrisBench
     {
+        private readonly StringBuilder _builder = new(16);
+        private const string _test = "test";
+
+        //[Benchmark]
+        //public StringBuilder AppendStackallock()
+        //{
+        //    return _builder.Append(stackalloc char[] { 't', 'e', 's', 't' }).Clear();
+        //}
+
+        //[Benchmark]
+        //public StringBuilder AppendString() => _builder.Append("test").Clear();
+
+        [Benchmark]
+        public bool StartsWithString1()
+            => _test.AsSpan().StartsWith("test", StringComparison.OrdinalIgnoreCase);
+        
+        [Benchmark]
+        public bool StartsWithString2()
+            => _test.StartsWith("test", StringComparison.OrdinalIgnoreCase);
+
+
+        [Benchmark]
+        public bool StartsWithStackallock()
+            => _test.AsSpan().StartsWith(stackalloc char[] { 't', 'e', 's', 't' }, StringComparison.OrdinalIgnoreCase);
 
     }
 }
